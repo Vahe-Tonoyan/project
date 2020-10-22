5ba08d28c1f4777ecd259d1bafe5532c087b1ac7
Vahe <tonoyanvahe@gmail.com>
2020-10-22 14:32:28 +0400

    my first commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 === 8);
+
+const isCheched = false,
+      isClose = false;
+
+console.log(isCheched || !isClose);
