f0892c253491249c35283a4ab36c5a9fdeb9534c
Vahe <tonoyanvahe@gmail.com>
2020-10-23 12:34:14 +0400

    Films questions

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
-"use strict";
 
-let incr = 10,
-    decr = 10;
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
 
-// incr++;
-// decr--;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-console.log(++incr);
-console.log(--decr);
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-console.log(5%2);
-
-console.log(2 + 2 * 2 === 8);
-
-const isCheched = false,
-      isClose = false;
-
-console.log(isCheched || !isClose);
+console.log(personalMovieDB);
